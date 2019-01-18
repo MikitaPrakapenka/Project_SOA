83ea165de72cb3668b40cf731e92a3f2d2f125f7
Mikitos <prakapenka.mikita@yahoo.com>
2019-01-18 15:15:11 +0100

    Updates

--- src/main/java/Gestion_thermo/Gestion_Thermo.java
+++ src/main/java/Gestion_thermo/Gestion_Thermo.java
@@ -1,12 +1,15 @@
 package Gestion_thermo;
 
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import fr.insa.soa.RestProject.Etudiant;
 import insat.fr.Salle;
 
 @Path("gestion_thermo")
@@ -16,14 +19,32 @@ public class Gestion_Thermo {
 	
 	
 	@GET
-	@Path("/get_temp/{idSalle}")
+	@Path("/get_temp_in/{idSalle}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Salle get_temp (@PathParam("idSalle") int id, Object idSalle) {
+	public Salle get_temp_in (@PathParam("idSalle") int id, Object idSalle) {
 		
-		Salle.getTemperature(idSalle);
 		
-		return etudiant;
+		return Salle.getTemperature(idSalle);
 	}
+	
+	
+	@GET
+	@Path("/get_temp_out")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Salle get_temp_out () {
+		
+		
+		return getTemperature_out());
+	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public void (Etudiant etudNouv) {
+		System.out.println("Ajout de l'etudiant  "+etudNouv.getNom()+" "+etudNouv.getPrenom()+" ok" + " et puis Ajout de l'etudiant  "+etudNouv.getBinome().getNom()+" "+etudNouv.getBinome().getPrenom()+" ok");
+
+	}
+	
+	
 	
 	
 	
